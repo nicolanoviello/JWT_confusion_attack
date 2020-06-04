aad58fcf69845e3a1cd1004caa14e0bcd5e28ed6
Nico <nico@iMac-27-7piano.homenet.telecomitalia.it>
2020-06-05 00:42:07 +0200

    HMAC Key

---
+++ jwtlib.py
@@ -0,0 +1,52 @@
+from functools import wraps
+from flask import request, Response
+import json
+import jwt 
+
+signing_key="abcd"
+
+def encode_auth_token(utenteloggato):
+    try:
+        if  utenteloggato.ruolo == 'abcde':
+            payload = {
+            'username': utenteloggato.username,
+            'role': 'root'
+            }
+        elif utenteloggato.ruolo == 'root':
+            payload = {
+            'username': utenteloggato.username,
+            'role': 'fake_root'
+            }
+        else:
+            payload = {
+                'username': utenteloggato.username
+            }
+        jwt_encoded = jwt.encode(payload,signing_key,algorithm='HS256')
+        return jwt_encoded.decode()
+    except Exception as e:
+        return e
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth_token = False
+        if not auth_token:
+            auth_token = request.headers.get('capstoneAuth')
+        if not auth_token:
+            auth_token = request.headers.get('Authorization')
+        if not auth_token:
+            auth_token = request.cookies.get('capstoneAuth')
+        if not auth_token:  # Authtoken no present so send 401
+            return Response('Token mancante!\n' 'Mancano le autorizzazioni per effettuare la chiamata', 401,
+                            {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        else:
+            return f(*args, **kwargs)
+    return decorated
+
+def decode_auth_token(token):
+    try:
+        token = token.replace("Bearer ",'')
+        message_received = jwt.decode(token, signing_key, algorithms=['HS256'])
+        return(message_received)
+    except Exception as e:
+        return e
---
+++ resources.py
@@ -0,0 +1,82 @@
+from flask_restful import Resource, reqparse
+from models import UserModel
+from flask import jsonify
+#import jwt
+import flask
+from flask import request, Response
+from jwtlib import encode_auth_token, decode_auth_token, requires_auth
+
+# flask_restful ha una libreria integrata per il parsing
+# possiamo usarla per effettuare verifiche sui campi obbligatori
+# ad esempio questi sono quelli necessari alla login
+parser = reqparse.RequestParser()
+parser.add_argument('username', help = 'Questo campo non può essere vuoto', required = True)
+parser.add_argument('password', help = 'Questo campo non può essere vuoto', required = True)
+parser.add_argument('ruolo', help = 'Questo campo non può essere vuoto', required = False)
+
+
+# qui è definita la lista di tutti gli endpoint da provare
+class Registration(Resource):
+    def post(self):
+        data = parser.parse_args()
+        # Acquisisco i dati da JSON
+        nuovo_utente = UserModel(
+            username = data['username'],
+            password = data['password'],
+            ruolo = data['ruolo']
+        )
+        # Controllo se l'utente è già presente su db
+        if UserModel.cerca_su_db(data['username']):
+          return {'message': '{} è già presente sul DB'. format(data['username'])}
+
+        try:
+            # Provo a creare un nuovo utente con i dati inviati nella post
+            nuovo_utente.salva_sul_db()
+        except:
+            # Restituisco un errore 500
+            return {'message': 'OPS. Qualcosa è andato storto.'}, 500
+        return data
+
+
+class Login(Resource):
+    def post(self):
+        data = parser.parse_args()
+        # Memorizzo i dati dell'utente in una variabile
+        utente_loggato = UserModel.cerca_su_db(data['username'])
+        # Controllo se l'utente è presente sul DB
+        if not utente_loggato:
+            return {'message': '{} non esiste'.format(data['username'])}
+        
+        if data['password'] == utente_loggato.password:
+            
+            #payload = {'username':utente_loggato.username, 'role':utente_loggato.ruolo}
+            encode_jwt = encode_auth_token(utente_loggato)
+            return {
+                'message': 'Hai effettuato l\'accesso come {}'.format(utente_loggato.username),
+                'auth_token':encode_jwt
+                }
+        else:
+            return {'message': 'Credenziali sbagliate'}
+      
+      
+class ListaUtenti(Resource):
+    def get(self):
+        return UserModel.tutta_la_lista()
+
+    def delete(self):
+        UserModel.svuota_db()
+        return {'message': 'Ripulisco il db cancellando tutti gli utenti'}
+      
+      
+class CheckJWT(Resource):
+    @requires_auth
+    def get(self):
+        auth_token = request.headers.get('Authorization')
+        print(auth_token)
+        message_received = decode_auth_token(auth_token)
+        if not message_received.get('role'):
+            return {'message': 'Benvenuto studente!'},200
+        if message_received.get('role') == 'root':
+            return {'message': 'Sei ufficialmente root'},200
+        elif message_received.get('role') == 'fake_root':
+            return {'message': 'Mi dispiace per te ma sei un fake root'},200
